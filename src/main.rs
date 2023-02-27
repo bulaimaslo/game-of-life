54e0f8a435cb9edfa66652a2e60dfe01de5c64db
Adam Neumann <57113336+bulaimaslo@users.noreply.github.com>
2023-02-27 22:24:14 +0100

    Added game board

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,45 @@
 use macroquad::prelude::*;
 
-#[macroquad::main("BasicShapes")]
+const SIZE: u16 = 20;
+
+#[macroquad::main("GameOfLife")]
 async fn main() {
+    let mut board = [false; (SIZE * SIZE) as usize];
+
+    board[0] = true;
+    board[21] = true;
+    board[42] = true;
+    board[63] = true;
+    board[84] = true;
+
     loop {
         clear_background(RED);
 
-        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
-        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
-        draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
+        for i in 0..SIZE * SIZE {
+            let row = i / SIZE;
+            let col = i % SIZE;
 
-        draw_text("IT WORKS!", 20.0, 20.0, 30.0, DARKGRAY);
+            draw_rectangle(
+                screen_width() / SIZE as f32 * col as f32,
+                screen_height() / SIZE as f32 * row as f32,
+                screen_width() / SIZE as f32,
+                screen_height() / SIZE as f32,
+                if board[i as usize] { BLACK } else { WHITE },
+            );
+        }
+
+        if is_key_down(KeyCode::Space) {
+            break;
+        }
 
         next_frame().await
     }
+}
+
+fn get_cell_state(board: &[bool], row: u16, col: u16) -> bool {
+    if col >= SIZE || row >= SIZE {
+        panic!("Out of bounds");
+    }
+
+    board[(row * SIZE + col) as usize]
 }
